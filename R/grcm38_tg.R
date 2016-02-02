e42a2c14db7141653b5586301a5083632802b147
Stephen Turner <vustephen@gmail.com>
2016-02-02 17:24:50 -0500

    add transcripts-to-gene for m38 and h38 fixes #2

---
+++ R/grcm38_tg.R
@@ -0,0 +1,9 @@
+#' Mouse GRCm38.p4 genes-to-transcripts
+#'
+#' Mouse GRCm38.p4 Ensembl transcript IDs to gene IDs from Ensembl release 82.
+#'
+#' @examples
+#' head(grcm38_tg)
+#'
+#' @source \url{http://useast.ensembl.org/Mus_musculus}
+"grcm38_tg"
